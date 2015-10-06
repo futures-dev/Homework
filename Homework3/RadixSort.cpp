8b7c48f483d077586b9dab183ca33a79365348d2
Andrei@Computer <futures_dev@outlook.com>
2015-10-06 23:24:58 +0300

    TODO: sort_at_digit

--- Homework3/RadixSort.cpp
+++ Homework3/RadixSort.cpp
@@ -16,11 +16,33 @@ int get_digit(int number, int base, int digit_index) {
     return number%base;
 }
 
-int main(){
-   cout << get_digit(143,10,1);
-    cout << get_digit(8,3,0);
-}
 
 void sort_at_digit(int *arr, int n, int base, int digit_index) {
+    int digitCount[base]; // initialize to zeros
+    memset(digitCount,0,base*sizeof(int));
+    int digits[n];
+    for (int i = 0;i<n;i++)
+        digitCount[digits[n]  = get_digit(arr[i],base,digit_index)]++; // ADD COMMENT
+    int count = 0;
+    for (int i = 0;i<base;i++){
+        int tmp = digitCount[i];
+        digitCount[i] = count;
+        count += tmp;
+    }
+    int outArray[n];
+    for (int i = 0;i<n;i++){
+        int d = get_digit(arr[i],base,digit_index);
+        outArray[digitCount[d]++] = arr[i];
+    }
+    delete[] arr;
+    arr = outArray;
+    delete[] (outArray,digitCount,digits);
+}
 
+int main(){
+   int* b = new int[7] {33,45,21,43,26,73,25};
+    sort_at_digit(b,7,10,0);
+    for (int i = 0;i<7;i++)
+        cout<<b[i]<<'\n';
+    delete[] b;
 }

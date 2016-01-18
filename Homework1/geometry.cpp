92cd8e15f4d4cd95600e56d8598f4b044eee04dc
Andrei <futures_dev@outlook.com>
2016-01-18 16:36:09 +0300

    Homework12

--- Homework1/geometry.cpp
+++ Homework1/geometry.cpp
@@ -2,6 +2,8 @@
 // Created by Andrei on 18.01.2016.
 //
 
+#include "geometry.h"
+
 Point::Point(int x, int y) {
     setX(x);
     setY(y);
@@ -24,27 +26,36 @@ void Point::setY(const int new_y) {
 }
 
 PointArray::PointArray() {
-
+    PointArray(nullptr, 0);
 }
 
-PointArray::PointArray(const ::PointArray::Point *points, const int size) {
-
+PointArray::PointArray(const Point *points, const int size) {
+    _storage = new Point[size];
+    for (int i = 0; i < size; i++) {
+        _storage[i] = points[i];
+    }
 }
 
-PointArray::PointArray(const ::PointArray::PointArray &pv) {
-
+PointArray::PointArray(const PointArray &pv) {
+    PointArray(pv._storage, pv._size);
 }
 
 PointArray::~PointArray() {
-
+    delete _storage;
 }
 
-void PointArray::push_back(const ::PointArray::Point &p) {
-
+void PointArray::push_back(const Point &p) {
+    resize(_size + 1);
+    _storage[_size++] = p;
 }
 
-void PointArray::insert(const int position, const ::PointArray::Point &p) {
-
+void PointArray::insert(const int position, const Point &p) {
+    resize(_size + 1);
+    for (int i = _size; i > position; i--) {
+        _storage[i] = _storage[i - 1];
+    }
+    _storage[position] = p;
+    _size++;
 }
 
 void PointArray::remove(const int position) {
@@ -52,7 +63,7 @@ void PointArray::remove(const int position) {
 }
 
 const int PointArray::getSize() const {
-    return 0;
+    return _size;
 }
 
 void PointArray::clear() {
@@ -60,11 +71,11 @@ void PointArray::clear() {
 }
 
 Point *PointArray::get(const int position) {
-    return nullptr;
+    return (&_storage)[position];
 }
 
 const Point *PointArray::get(const int position) const {
-    return nullptr;
+    return (&_storage)[position];
 }
 
 void PointArray::resize(int n) {

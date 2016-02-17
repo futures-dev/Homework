4b2003603df070b96773750e5b8b71b72b54c03a
Andrei <futures_dev@outlook.com>
2016-02-17 21:56:53 +0300

    hw5 done

--- Homework5/NiceQueue.hpp
+++ Homework5/NiceQueue.hpp
@@ -1,42 +1,62 @@
 //
 // Andrei Kolomiets 143-1
-// CLion 1.2 MinGW 3.4.1
-// 15.02.2016 
+// CLion 1.2 cpp 5.2.1
+// 17.02.2016
+//
+
 #ifndef HOMEWORK_NICEQUEUE_HPP
 #define HOMEWORK_NICEQUEUE_HPP
 
-
+/*
+ * Constructs a NiceQueue obejct with given capacity
+ */
 template<class T>
 NiceQueue<T>::NiceQueue(size_t capacity) : m_capacity(capacity), inStack(capacity), outStack(capacity) { }
 
+/*
+ * Returns number of elements in the queue
+ */
 template<class T>
 size_t NiceQueue<T>::size(void) const {
     return inStack.size() + outStack.size();
 }
 
+/*
+ * Inserts an element into the queue
+ */
 template<class T>
 void NiceQueue<T>::enq(T newelement) throw(out_of_range) {
     // out of range exception will be thrown in stack if relevant
     inStack.push(newelement);
 }
 
+/*
+ * Returns first element of the queue and removes it
+ */
 template<class T>
 T NiceQueue<T>::deq(void) throw(out_of_range) {
+    // first, try to pop outStack
     if (outStack.size() > 0) {
         return outStack.pop();
     }
     else {
+        // then, move elements from inStack to outStack
         int n = inStack.size();
         for (int i = 0; i < n; i++) {
             outStack.push(inStack.pop());
         }
+        // finally, pop outStack
         // out of range exception will be thrown in stack if relevant
         return outStack.pop();
     }
 }
 
+/*
+ * Returns minimum of the queue elements
+ */
 template<class T>
 T NiceQueue<T>::getMinimum(void) throw(out_of_range) {
+    // minimum of inStack and outStack minima
     if (outStack.size() == 0) {
         if (inStack.size() == 0) {
             throw out_of_range("Attempt to get minimum of an empty queue");
--- Homework5/NiceStack.hpp
+++ Homework5/NiceStack.hpp
@@ -1,55 +1,77 @@
 //
 // Andrei Kolomiets 143-1
-// CLion 1.2 MinGW 3.4.1
-// 15.02.2016 
+// CLion 1.2 cpp 5.2.1
+// 17.02.2016
 //
 
 #ifndef HOMEWORK_NICESTACK_HPP
 #define HOMEWORK_NICESTACK_HPP
 
+/*
+ * Constructs a NiceStack with given capacity
+ */
 template<class T>
 NiceStack<T>::NiceStack(size_t capacity) : iHead(0), m_capacity(capacity), storage(capacity) { }
 
+/*
+ * Returns current stack size
+ */
 template<class T>
 size_t NiceStack<T>::size() const {
     return iHead;
 }
 
+/*
+ * Destructor not required because storage vector is a non-pointer member
+ */
 template<class T>
-NiceStack<T>::~NiceStack(void) {
-}
+NiceStack<T>::~NiceStack(void) { }
 
+/*
+ * Inserts an element in the stack
+ */
 template<class T>
 void NiceStack<T>::push(T newelement) throw(out_of_range) {
     if (size() == m_capacity) {
         throw out_of_range("Attempt to push into a full stack.");
     }
     if (size() == 0) {
+        // current minimum is the only element
         storage.push_back(TypeElementStack(newelement, newelement));
     }
     else {
+        // current minimum is min(current minimum, new elem)
         storage.push_back(
                 TypeElementStack(newelement, newelement < storage.back().second ? newelement : storage.back().second));
     }
     iHead++;
 }
 
+/*
+ * Returns an element in the end of the stack and removes it
+ */
 template<class T>
 T NiceStack<T>::pop(void) throw(out_of_range) {
     if (size() == 0) {
         throw out_of_range("Attempt to pop from an empty stack.");
     }
+    // value stored in first
     T v = storage.back().first;
+    // remove
     storage.pop_back();
     iHead--;
     return v;
 }
 
+/*
+ * Return minimum of the stack elements
+ */
 template<class T>
 T NiceStack<T>::getMinimum(void) throw(out_of_range) {
     if (size() == 0) {
         throw out_of_range("Attempt to get minimum of an empty stck.");
     }
+    // no remove
     return storage.back().second;
 }
 
